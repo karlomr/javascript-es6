540c0cfbee1cb5db435c4bfd558dbdcb07b887c5
karlo <karlomr@gmail.com>
2020-05-28 14:02:58 -0300

    First commit

---
+++ src/main_old01.js
@@ -0,0 +1,11 @@
+// import soma, {sub as Subtrai} from './funcoes';
+import * as funcoes from './funcoes'
+import mult from './multiplicacao';
+
+
+// console.log(soma(1,2));
+// console.log(Subtrai(4,2));
+
+console.log(funcoes.soma(1,2));
+console.log(funcoes.sub(4,2));
+//console.log(mult(2,9));
